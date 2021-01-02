4b0a6954c7bb9aba120f8d7392b344dbe5d8b833
SK <skthetester@gmail.com>
2021-01-02 18:42:35 -0500

    Add Magic 8 Ball

---
+++ magic8ball.py
@@ -0,0 +1,11 @@
+import random
+
+print("Welcome to Magic 8!")
+magic8_answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.",
+                  "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.",
+                  "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
+                  "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.",
+                  "My sources say no.", "Outlook not so good.", "Very doubtful."]
+
+response = input("Think of a question and hit ENTER")
+print("Magic 8 Ball says... ", random.choice(magic8_answers))
